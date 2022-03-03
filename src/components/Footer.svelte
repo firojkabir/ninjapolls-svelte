4774ed09eade0593b48cd51b11caced602a2dc6f
Md Firoj Kabir <firoj.kabir65@gmail.com>
2022-03-03 19:37:18 +0100

    Add header and footer components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,25 @@
+<footer>
+  <div class="copyright">
+    &copy; Copyright
+    <span id="year">
+      <script type="text/javascript">
+        document.getElementById("year").innerHTML = new Date().getFullYear();
+      </script> - Poll Ninja
+    </span>
+  </div>
+</footer>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+</style>
