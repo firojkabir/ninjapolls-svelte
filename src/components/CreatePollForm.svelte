131069cefbfb05d2637d064cac226a33d6fda957
Md Firoj Kabir <firoj.kabir65@gmail.com>
2022-03-04 11:50:17 +0100

    Add custom form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -2,9 +2,37 @@
   import Button from "../shared/Button.svelte";
 
   let fields = { question: "", answerA: "", answerB: "" };
+  let errors = { question: "", answerA: "", answerB: "" };
+  let valid = false;
 
   const submitHandler = () => {
-    console.log(fields);
+    valid = true;
+    //validate question
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long";
+    } else {
+      errors.question = "";
+    }
+    //validate answer A
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A can not be empty!";
+    } else {
+      errors.answerA = "";
+    }
+    //validate answer B
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B can not be empty!";
+    } else {
+      errors.answerB = "";
+    }
+
+    //add new poll
+    if (valid) {
+      console.log("valid", fields);
+    }
   };
 </script>
 
@@ -12,14 +40,17 @@
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA} />
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
+    <div class="error">{errors.answerB}</div>
   </div>
   <Button type="secondary" flat={true}>Add Poll</Button>
 </form>
@@ -43,5 +74,11 @@
   label {
     margin: 10px auto;
     text-align: left;
+  }
+
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
 </style>
