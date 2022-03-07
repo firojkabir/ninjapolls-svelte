8f121dd7ff78b6ec1d0ec4cbadc11ad3f1fce1d3
Md Firoj Kabir <firoj.kabir65@gmail.com>
2022-03-07 09:47:20 +0100

    Add basic transition

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,10 +1,11 @@
 <script>
   //   import { onDestroy, onMount } from "svelte";
-
+  import { fade, slide, scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
   import PollStore from "../stores/PollStore";
   import PollDetails from "./PollDetails.svelte";
 
-  export let polls = [];
+  //   export let polls = [];
 
   //   const unsub = PollStore.subscribe((data) => {
   //     polls = data;
@@ -18,7 +19,7 @@
 
 <div class="poll-list">
   {#each $PollStore as poll (poll.id)}
-    <div>
+    <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
       <PollDetails {poll} />
     </div>
   {/each}
